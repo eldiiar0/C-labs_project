cbf6daf0b8651e6f71b057b608a2972469e4ab14
eldiiar0 <115143883+eldiiar0@users.noreply.github.com>
2023-12-11 15:21:11 +0600

    Add files via upload

---
+++ Task manager/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Task_manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleTbox = new System.Windows.Forms.TextBox();
+            this.descriptionTbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.listView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Navy;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(-2, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(808, 95);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Task Manager";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // titleTbox
+            // 
+            this.titleTbox.Location = new System.Drawing.Point(23, 142);
+            this.titleTbox.Name = "titleTbox";
+            this.titleTbox.Size = new System.Drawing.Size(288, 20);
+            this.titleTbox.TabIndex = 1;
+            // 
+            // descriptionTbox
+            // 
+            this.descriptionTbox.Location = new System.Drawing.Point(23, 212);
+            this.descriptionTbox.Multiline = true;
+            this.descriptionTbox.Name = "descriptionTbox";
+            this.descriptionTbox.Size = new System.Drawing.Size(288, 214);
+            this.descriptionTbox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Agency FB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.Location = new System.Drawing.Point(17, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Title";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.Navy;
+            this.clearButton.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.ForeColor = System.Drawing.Color.White;
+            this.clearButton.Location = new System.Drawing.Point(23, 485);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(288, 37);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            // 
+            // editButton
+            // 
+            this.editButton.BackColor = System.Drawing.Color.Navy;
+            this.editButton.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editButton.ForeColor = System.Drawing.Color.White;
+            this.editButton.Location = new System.Drawing.Point(126, 442);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(84, 37);
+            this.editButton.TabIndex = 6;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = false;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.Navy;
+            this.deleteButton.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteButton.ForeColor = System.Drawing.Color.White;
+            this.deleteButton.Location = new System.Drawing.Point(227, 442);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(84, 37);
+            this.deleteButton.TabIndex = 7;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.Navy;
+            this.saveButton.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.ForeColor = System.Drawing.Color.White;
+            this.saveButton.Location = new System.Drawing.Point(23, 442);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(84, 37);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // listView
+            // 
+            this.listView.AllowUserToOrderColumns = true;
+            this.listView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.listView.BackgroundColor = System.Drawing.Color.White;
+            this.listView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listView.Location = new System.Drawing.Point(334, 142);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(448, 380);
+            this.listView.TabIndex = 9;
+            this.listView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.listView_CellContentClick);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BackColor = System.Drawing.Color.Navy;
+            this.label4.Font = new System.Drawing.Font("Gill Sans MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(-2, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(808, 23);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Your To Do List ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Agency FB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Navy;
+            this.label5.Location = new System.Drawing.Point(510, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 32);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Tasks";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Agency FB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Navy;
+            this.label6.Location = new System.Drawing.Point(17, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 32);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Description";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BackColor = System.Drawing.Color.Navy;
+            this.label2.Font = new System.Drawing.Font("Agency FB", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(-2, 536);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(808, 14);
+            this.label2.TabIndex = 13;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(805, 551);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.descriptionTbox);
+            this.Controls.Add(this.titleTbox);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Task Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.listView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox titleTbox;
+        private System.Windows.Forms.TextBox descriptionTbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.DataGridView listView;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Task manager/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Task_manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataTable taskList = new DataTable();
+        bool isEditing = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            taskList.Columns.Add("Title");
+            taskList.Columns.Add("Description");
+            taskList.Columns.Add(new DataColumn("Done", typeof(bool)));
+            listView.DataSource = taskList;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+ 
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            titleTbox.Text = "";
+            descriptionTbox.Text = "";
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            isEditing= true;
+            titleTbox.Text = taskList.Rows[listView.CurrentCell.RowIndex].ItemArray[0].ToString();
+            descriptionTbox.Text = taskList.Rows[listView.CurrentCell.RowIndex].ItemArray[1].ToString();
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                taskList.Rows[listView.CurrentCell.RowIndex].Delete();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (isEditing)
+            {
+                taskList.Rows[listView.CurrentCell.RowIndex]["Title"] = titleTbox.Text;
+                taskList.Rows[listView.CurrentCell.RowIndex]["Description"] = descriptionTbox.Text;
+            }
+            else
+            {
+                taskList.Rows.Add(titleTbox.Text, descriptionTbox.Text);
+
+            }
+            titleTbox.Text = "";
+            descriptionTbox.Text = "";
+            isEditing = false;
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
